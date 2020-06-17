8ad1684b5aa25288130a09749b7758bb6c625d50
Colin Nelson <colinnelson1996@gmail.com>
2020-06-16 19:51:36 -0600

    current with catcollector week 8 day 2

---
+++ main_app/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Dog(models.Model):
+    name = models.CharField(max_length=50)
+    breed = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    age = models.IntegerField()
---
+++ main_app/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from .models import Dog
+
+# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def dogs_index(request):
+    dogs = Dog.objects.all()
+    return render(request, 'dogs/index.html', { 'dogs': dogs })
+
+def dogs_detail(request, dog_id):
+    dog = Dog.objects.get(id=dog_id)
+    return render(request, 'dogs/detail.html', {'dog': dog})
