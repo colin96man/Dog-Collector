57ef883bcb11d9047fd7b4b8a5594187a97364f9
Colin Nelson <colinnelson1996@gmail.com>
2020-06-17 21:12:31 -0600

    update and delete functionality & Feeding model

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Dog
 
 # Create your views here.
@@ -21,3 +21,10 @@ class DogCreate(CreateView):
     model = Dog
     fields = '__all__'
 
+class DogUpdate(UpdateView):
+    model = Dog
+    fields = ['breed', 'description', 'age']
+
+class DogDelete(DeleteView):
+    model = Dog
+    success_url = '/dogs/'
