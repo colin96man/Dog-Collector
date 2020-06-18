3701d443c8749d89bc2d7fefc0f7e8a14da07031
Colin Nelson <colinnelson1996@gmail.com>
2020-06-17 20:44:39 -0600

    create dog functionality

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Dog(models.Model):
@@ -6,3 +7,10 @@ class Dog(models.Model):
     breed = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
     age = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'dog_id': self.id})
+
